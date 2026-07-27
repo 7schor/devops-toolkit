724fc5273403ad815aaef1b9d8dfdfca12c74f98
7schor <timschorling@protonmail.com>
2026-07-27 20:26:10 +0200

    Refactor program into reusable functions

--- toolbox.py
+++ toolbox.py
@@ -1,6 +1,29 @@
+def greet(person):
+    if person == "":
+        print("You did not enter a name.")
+    elif person == "Tim":
+        print("Welcome back, Tim!")   
+    elif person == "Anja":
+        print("Welcome back, Anja!")
+    else:
+        print(f"Hello, {person}! Welcome to the DevOps Toolkit!")
 
-def main():
+def get_name():
+   while True:
+        name = input("Please enter your name: ")
+
+        if name != "":
+            return name
+        
+        print("Please enter a name.")
+        
+
+def welcome():
     print("Welcome to the DevOps Toolkit!")
 
+def main():
+    welcome()
+    greet(get_name())
+
 if __name__ == "__main__":
-    main()    
+    main()
